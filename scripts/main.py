fbf7781f848a56efd1a03f25e503df7a6d79acfd
jabirjabzz <jabirmoh07@gmail.com>
2024-12-15 17:26:33 +0530

    newly added main

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,12 @@
+import os
 from scripts.clean_text import clean_text_file
 
 def main():
-    input_file = "data/raw_text.txt"
-    output_file = "data/cleaned_text.txt"
+    input_dir = r"C:\Users\Administrator\Documents\GitHub\Malayalam-Language-Scraping\output_Json"
+    output_file = os.path.join("C:\\Users\\Administrator\\Documents\\GitHub\\Text cleaning\\data\\output", "cleaned_data.jsonl")
     
-    print("Starting text cleaning...")
-    clean_text_file(input_file, output_file)
+    print("Starting Malayalam text cleaning...")
+    clean_text_file(input_dir, output_file)
     print(f"Text cleaning completed. Cleaned text saved to {output_file}")
 
 if __name__ == "__main__":
